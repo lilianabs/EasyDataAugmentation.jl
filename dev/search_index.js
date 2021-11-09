e8444f7441bcb3e54b5bf5489036bbab180a5003
Documenter.jl <documenter@juliadocs.github.io>
2021-11-09 14:03:57 +0000

    build based on ee8962a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = EasyDataAugmentationNLP","category":"page"},{"location":"#EasyDataAugmentationNLP","page":"Home","title":"EasyDataAugmentationNLP","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for EasyDataAugmentationNLP.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [EasyDataAugmentationNLP]","category":"page"}]
+}
