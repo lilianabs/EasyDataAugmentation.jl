339d6341ddc9f6fe8dcb17d84bcade4f3bdaaea4
lilianabs <lilianabsmath@google.com>
2021-11-23 11:49:04 -0600

    Add functions for cleaning text

--- src/generate_augmented_sentences.jl
+++ src/generate_augmented_sentences.jl
@@ -6,11 +6,24 @@ function easy_data_augmentation(input_file)
 
     open(input_file) do f
         for (i, line) in enumerate(eachline(f))
-            label, text = split(line, "\t")
-            tokens = split(text, " ")
-            println("Line $i: $label, $text, $tokens")
+            label, sentence = split(line, "\t")
+            aug_sentences = augment_sentence(sentence)
+            #println("Line $i: $label, $sentence")
+            #augment sentence
         end
     end
 
     return input_file
+end
+
+
+function augment_sentence(sentence)
+    words = split(sentence, " ")
+
+    return []
+
+end
+
+function clean_text_sentence(sentence)
+
 end
